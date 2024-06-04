1fd58ffdeb1ca86eebda468875f35cef3ab765bc
Robert <robertbroersma@gmail.com>
2024-06-04 15:23:40 +0200

    Clean up register.vue

--- pages/register.vue
+++ pages/register.vue
@@ -22,6 +22,8 @@ import { Button } from "@/components/ui/button"
 const mapCenter = ref([0, 0])
 import Label from "@/components/ui/label/Label.vue"
 import CustomDropDownComponent from "@/components/CustomDropDownComponent.vue"
+import Container from "~/components/deltares/Container.vue"
+import Textarea from "~/components/ui/textarea/Textarea.vue"
 
 const accesToken =
   "pk.eyJ1IjoicGlldGVyZ3JpanplMTIzIiwiYSI6ImNreGc2emtjcjNtYmkycm81czF3M3Zpa3YifQ.ZJEb13EmlPZwXY5PCp80sw"
@@ -70,151 +72,171 @@ const dateDataSet = ref<DateValue>()
 </script>
 
 <template>
-  <Label class="text-3xl flex justify-center p-5">Register your data</Label>
-  <Card>
-    <CardHeader>Data set collection</CardHeader>
-    <CardContent>
-      <CustomDropDownComponent
-        id="language"
-        :options="frameworks"
-        v-model="value"
-      />
-    </CardContent>
-    <Card>
-      <CardHeader>General information</CardHeader>
-      <CardContent>
-        <div class="grid grid-cols-[15%_85%] gap-5 p-5">
-          <Label for="projectnumber">Project number</Label>
-          <Input id="projectnumber" type="text" />
-          <Label for="projecttitle">Project title</Label>
-          <Input id="projecttitle" type="text" />
-          <Label for="dateDataSet">Date data set</Label>
-          <Popover>
-            <PopoverTrigger as-child>
-              <Button
-                id="dateDataSet"
-                variant="outline"
-                :class="
-                  cn(
-                    'w-[280px] justify-start text-left font-normal',
-                    !value && 'text-muted-foreground',
-                  )
-                "
-              >
-                <CalendarIcon class="mr-2 h-4 w-4" />
-                {{
-                  dateDataSet
-                    ? df.format(dateDataSet.toDate(getLocalTimeZone()))
-                    : "Pick a date"
-                }}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent class="w-auto p-0">
-              <Calendar v-model="dateDataSet" initial-focus />
-            </PopoverContent>
-          </Popover>
-          <Label class="m-2" for="datum type">Datum type</Label>
-          <CustomDropDownComponent
-            id="datum type"
-            :options="datumTypes"
-            v-model="selectedDatumType"
-          />
-          <Label class="m-2" for="status">Status</Label>
-          <CustomDropDownComponent
-            id="status"
-            :options="statusTypes"
-            v-model="selectedStatus"
-          />
-          <Label class="m-2" for="language">Language</Label>
+  <Container class="py-8">
+    <h1 class="text-3xl flex font-semibold">Register your data</h1>
+
+    <div class="mt-8 grid grid-flow-row gap-5">
+      <Card>
+        <CardHeader
+          ><CardTitle class="text-lg"
+            >Data set collection</CardTitle
+          ></CardHeader
+        >
+        <CardContent>
           <CustomDropDownComponent
             id="language"
-            :options="languages"
-            v-model="selectedLanguage"
+            :options="frameworks"
+            v-model="value"
           />
-          <Label class="m-2" for="description">Description</Label>
-          <textarea class="m-2" id="description" />
-          <Label class="m-2" for="storagelocation">Storage location</Label>
-          <Input class="m-2" id="storagelocation" type="text" />
-        </div>
-      </CardContent>
-    </Card>
-    <Card>
-      <CardHeader>Keywords</CardHeader>
-      <CardContent>
-        <div class="grid grid-cols-[15%_85%] gap-5 p-5">
-          <Label for="keywords" class="m-2">Keywords</Label>
-          <Input id="keywords"></Input>
-        </div>
-      </CardContent>
-    </Card>
-    <Card>
-      <CardHeader>Restrictions</CardHeader>
-      <CardContent>
-        <div class="grid grid-cols-[15%_85%] gap-5 p-5">
-          <Label class="m-2" for="accescontraints">Access constraints</Label>
-          <CustomDropDownComponent
-            id="accescontraints"
-            :options="accessConstraints"
-            v-model="selectedAccessConstraint"
-          />
-        </div>
-        <div class="grid grid-cols-[15%_85%] gap-5 p-5">
-          <Label class="m-2" for="limitusage">Usage limits</Label>
-          <Input id="limitusage" />
-        </div>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
 
-    <Card>
-      <CardHeader>Spatial information</CardHeader>
-      <CardContent>
-        <div class="grid grid-cols-[15%_85%] gap-5 p-5">
-          <Label class="m-2" for="spatialreference"
-            >Spatial reference system</Label
+      <Card>
+        <CardHeader
+          ><CardTitle class="text-lg"
+            >General information</CardTitle
+          ></CardHeader
+        >
+        <CardContent>
+          <div
+            class="grid grid-cols-[max-content_1fr] gap-5 items-center items-center"
           >
-          <CustomDropDownComponent
-            id="spatialreference"
-            :options="spatialReferences"
-            v-model="selectedSpatialReference"
-          />
-          <Label class="m-2" for="map">Geometry</Label>
-          <div>
-            <MapboxMap
-              id="map"
-              style="height: 400px; width: 600px"
-              :access-token="accesToken"
-              map-style="mapbox://styles/mapbox/streets-v11"
-              :center="mapCenter"
-              :zoom="1"
+            <Label for="projectnumber">Project number</Label>
+            <Input id="projectnumber" type="text" />
+            <Label for="projecttitle">Project title</Label>
+            <Input id="projecttitle" type="text" />
+            <Label for="dateDataSet">Date data set</Label>
+            <Popover>
+              <PopoverTrigger as-child>
+                <Button
+                  id="dateDataSet"
+                  variant="outline"
+                  :class="
+                    cn(
+                      'w-[280px] justify-start text-left font-normal',
+                      !value && 'text-muted-foreground',
+                    )
+                  "
+                >
+                  <CalendarIcon class="mr-2 h-4 w-4" />
+                  {{
+                    dateDataSet
+                      ? df.format(dateDataSet.toDate(getLocalTimeZone()))
+                      : "Pick a date"
+                  }}
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent class="w-auto p-0">
+                <Calendar v-model="dateDataSet" initial-focus />
+              </PopoverContent>
+            </Popover>
+            <Label for="datum type">Datum type</Label>
+            <CustomDropDownComponent
+              id="datum type"
+              :options="datumTypes"
+              v-model="selectedDatumType"
             />
+            <Label for="status">Status</Label>
+            <CustomDropDownComponent
+              id="status"
+              :options="statusTypes"
+              v-model="selectedStatus"
+            />
+            <Label for="language">Language</Label>
+            <CustomDropDownComponent
+              id="language"
+              :options="languages"
+              v-model="selectedLanguage"
+            />
+            <Label for="description">Description</Label>
+            <Textarea id="description" />
+            <Label for="storagelocation">Storage location</Label>
+            <Input id="storagelocation" type="text" />
           </div>
-        </div>
-      </CardContent>
-    </Card>
-    <Card>
-      <CardHeader>Numerical models</CardHeader>
-      <CardContent>
-        <div class="grid grid-cols-[15%_85%] gap-5 p-5">
-          <Label for="assets" class="m-2">Assets</Label>
-          <Button
-            id="assets"
-            variant="outline"
-            class="w-[280px] justify-start text-left font-normal"
-          >
-            <PlusIcon class="w-4 h-4" />
-            Assets
-          </Button>
-          <Label for="properties" class="m-2">More properties</Label>
-          <Button
-            id="properties"
-            variant="outline"
-            class="w-[280px] justify-start text-left font-normal"
-          >
-            <PlusIcon class="w-4 h-4" />
-            Numerical models
-          </Button>
-        </div>
-      </CardContent>
-    </Card>
-  </Card>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader><CardTitle class="text-lg">Keywords</CardTitle></CardHeader>
+        <CardContent>
+          <div class="grid grid-cols-[max-content_1fr] gap-5 items-center">
+            <Label for="keywords">Keywords</Label>
+            <Input id="keywords"></Input>
+          </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader
+          ><CardTitle class="text-lg">Restrictions</CardTitle></CardHeader
+        >
+        <CardContent>
+          <div class="grid grid-cols-[max-content_1fr] gap-5 items-center">
+            <Label for="accescontraints">Access constraints</Label>
+            <CustomDropDownComponent
+              id="accescontraints"
+              :options="accessConstraints"
+              v-model="selectedAccessConstraint"
+            />
+            <Label for="limitusage">Usage limits</Label>
+            <Input id="limitusage" />
+          </div>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader
+          ><CardTitle class="text-lg"
+            >Spatial information</CardTitle
+          ></CardHeader
+        >
+        <CardContent>
+          <div class="grid grid-cols-[max-content_1fr] gap-5 items-center">
+            <Label for="spatialreference">Spatial reference system</Label>
+            <CustomDropDownComponent
+              id="spatialreference"
+              :options="spatialReferences"
+              v-model="selectedSpatialReference"
+            />
+            <Label for="map">Geometry</Label>
+            <div>
+              <MapboxMap
+                id="map"
+                style="height: 400px; width: 600px"
+                :access-token="accesToken"
+                map-style="mapbox://styles/mapbox/streets-v11"
+                :center="mapCenter"
+                :zoom="1"
+              />
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader
+          ><CardTitle class="text-lg">Numerical models</CardTitle></CardHeader
+        >
+        <CardContent>
+          <div class="grid grid-cols-[max-content_1fr] gap-5 items-center">
+            <Label for="assets">Assets</Label>
+            <Button
+              id="assets"
+              variant="outline"
+              class="w-fit justify-start text-left font-normal"
+            >
+              <PlusIcon class="w-4 h-4 mr-2" />
+              Assets
+            </Button>
+            <Label for="properties">More properties</Label>
+            <Button
+              id="properties"
+              variant="outline"
+              class="w-fit justify-start text-left font-normal"
+            >
+              <PlusIcon class="w-4 h-4 mr-2" />
+              Numerical models
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  </Container>
 </template>
