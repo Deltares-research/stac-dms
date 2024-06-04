2ded31643e02b2e27ea5a13986c7c1fd99e52fe0
Robert <robertbroersma@gmail.com>
2024-06-04 15:16:47 +0200

    Clean up items.vue

--- pages/items.vue
+++ pages/items.vue
@@ -1,90 +1,95 @@
 <script setup lang="ts">
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from '@/components/ui/table'
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
-import {TrashIcon, PencilIcon,PlusIcon} from 'lucide-vue-next'
+import Container from "@/components/deltares/Container"
+
+import { TrashIcon, PencilIcon, PlusIcon } from "lucide-vue-next"
 
 const items = [
-    {
-        title: 'Geo centrifuge',
-        description: 'Cool measurement',
-        creator: 'Andre Grijze',
-        collection: 'GeoCentrifuge',
-    },
-    {
-        title: 'Geo centrifuge',
-        description: 'Cooler measurement',
-        creator: 'Also Andre Grijze',
-        collection: 'GeoCentrifuge',
-    },
-    {
-        title: 'Geo centrifuge',
-        description: 'Bad measurement',
-        creator: 'Patrick',
-        collection: 'GeoCentrifuge',
-    },
-    {
-        title: 'Geo centrifuge',
-        description: 'Worst measurement',
-        creator: 'GertJan',
-        collection: 'GeoCentrifuge',
-    },
+  {
+    title: "Geo centrifuge",
+    description: "Cool measurement",
+    creator: "Andre Grijze",
+    collection: "GeoCentrifuge",
+  },
+  {
+    title: "Geo centrifuge",
+    description: "Cooler measurement",
+    creator: "Also Andre Grijze",
+    collection: "GeoCentrifuge",
+  },
+  {
+    title: "Geo centrifuge",
+    description: "Bad measurement",
+    creator: "Patrick",
+    collection: "GeoCentrifuge",
+  },
+  {
+    title: "Geo centrifuge",
+    description: "Worst measurement",
+    creator: "GertJan",
+    collection: "GeoCentrifuge",
+  },
 ]
 </script>
 
 <template>
-    <Card>
-        <CardHeader>Registrated data</CardHeader>
-        <CardContent>
+  <Container class="py-8">
+    <div class="flex items-center justify-between gap-5">
+      <h1 class="font-semibold text-2xl">Registered data</h1>
+      <Button as-child>
+        <NuxtLink to="/register" class="flex items-center gap-1">
+          <PlusIcon class="w-4 h-4 mr-2" /> Register new dataset
+        </NuxtLink>
+      </Button>
+    </div>
+    <Card class="mt-8">
+      <CardContent class="p-0 pb-5">
         <Table>
-            <TableCaption>List of data sets registrations, which you are allowed to edit</TableCaption>
-            <TableHeader>
-                <TableRow>
-                    <TableHead class="w-[100px]">
-                        Title
-                    </TableHead>
-                    <TableHead>Description</TableHead>
-                    <TableHead>Creator</TableHead>
-                    <TableHead class="text-right">
-                        Collection
-                    </TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                <TableRow v-for="item in items" :key="item.title">
-                    <TableCell class="font-medium">
-                        {{ item.title }}
-                    </TableCell>
-                    <TableCell>{{ item.description }}</TableCell>
-                    <TableCell>{{ item.creator }}</TableCell>
-                    <TableCell class="text-right">
-                        {{ item.collection }}
-                    </TableCell>
-                    <TableCell class="text-right">
-                        <Button>
-                            <PencilIcon class="w-4 h-4 mr-2"/>
-                        </Button>
-                    </TableCell>
-                    <TableCell class="text-right">
-                        <Button>
-                            <TrashIcon class="w-4 h-4 mr-2"/>
-                        </Button>
-                    </TableCell>
-                </TableRow>
-            </TableBody>
+          <TableCaption
+            >List of data sets registrations, which you are allowed to
+            edit</TableCaption
+          >
+          <TableHeader>
+            <TableRow>
+              <TableHead>Title</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>Creator</TableHead>
+              <TableHead>Collection</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow v-for="item in items" :key="item.title">
+              <TableCell class="font-medium">
+                {{ item.title }}
+              </TableCell>
+              <TableCell>{{ item.description }}</TableCell>
+              <TableCell>{{ item.creator }}</TableCell>
+              <TableCell>
+                {{ item.collection }}
+              </TableCell>
+              <TableCell>
+                <div class="flex gap-1.5 justify-end">
+                  <Button size="icon" variant="outline">
+                    <PencilIcon class="w-4 h-4" />
+                  </Button>
+                  <Button size="icon" variant="destructive">
+                    <TrashIcon class="w-4 h-4" />
+                  </Button>
+                </div>
+              </TableCell>
+            </TableRow>
+          </TableBody>
         </Table>
-            <CardFooter>
-                <Button  @click="navigateTo('/register')" >
-                    <PlusIcon class="w-4 h-4 mr-2" /> Register new dataset
-                </Button>
-            </CardFooter>
-        </CardContent>
+      </CardContent>
     </Card>
+  </Container>
 </template>
