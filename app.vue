7a8b270b650f7438a351168fbef9220637d77579
Robert <robertbroersma@gmail.com>
2024-06-14 08:51:47 +0200

    Styling (#1)
    * feedback demo
    
    * login.png added
    
    * Format, cleanup, move layout to app.vue
    
    * Design increment
    
    * Cleanup view.vue
    
    * Cleanup search (index.vue)
    
    * Remove containers from search page
    
    * Clean up items.vue
    
    * Clean up register.vue
    
    ---------
    
    Co-authored-by: grijzea <andre.grijze@deltares.nl>

--- app.vue
+++ app.vue
@@ -1,3 +1,32 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+const loginDone = ref(false)
+
+function login() {
+  loginDone.value = true
+}
+</script>
+
 <template>
+  <div class="grid grid-cols-2 py-2 px-4 border-b border-border">
+    <nav class="flex">
+      <Button as-child variant="link">
+        <NuxtLink to="/">Search</NuxtLink>
+      </Button>
+      <Button as-child v-if="loginDone" variant="link">
+        <NuxtLink to="/items">Register</NuxtLink>
+      </Button>
+      <Button as-child v-if="loginDone" variant="link">
+        <NuxtLink to="/main">Admin</NuxtLink>
+      </Button>
+      <Button as-child variant="link">
+        <NuxtLink to="/main">Storage Finder</NuxtLink>
+      </Button>
+    </nav>
+    <div class="flex justify-end">
+      <Button class="align-right" @click="login" variant="link">Login</Button>
+    </div>
+  </div>
   <NuxtPage />
 </template>
---
+++ pages/main.vue
@@ -0,0 +1,55 @@
+<template>
+  <Container>
+    <h3 class="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
+      Data management suite
+    </h3>
+    <p class="text-sm text-muted-foreground">
+      Welcome to the Deltares data management suite. Entry point for finding and
+      publishing datasets.
+    </p>
+    <div class="grid grid-cols-3 gap-4 pt-8">
+      <Card class="flex flex-col">
+        <CardHeader>
+          <CardTitle>Find data</CardTitle>
+          <CardDescription
+            >Explore the central data catalog using the radiantearth STAC
+            Browser.</CardDescription
+          >
+        </CardHeader>
+        <CardFooter class="mt-auto">
+          <Button>Search data</Button>
+        </CardFooter>
+      </Card>
+      <Card class="flex flex-col">
+        <CardHeader>
+          <CardTitle>Register data</CardTitle>
+          <CardDescription
+            >Register data for projects, experimental
+            facilities</CardDescription
+          >
+        </CardHeader>
+        <CardFooter class="mt-auto">
+          <Button>Register data</Button>
+        </CardFooter>
+      </Card>
+      <Card class="flex flex-col">
+        <CardHeader>
+          <CardTitle>Admin</CardTitle>
+          <CardDescription
+            >Adminstration of catalogues, collections and user
+            permissions</CardDescription
+          >
+        </CardHeader>
+        <CardFooter class="mt-auto">
+          <Button>Admin</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  </Container>
+</template>
+
+<script setup lang="ts">
+import Container from "~/components/deltares/Container.vue"
+</script>
+
+<style scoped></style>
