c21b28af47a8ccd999d6ad50c9bdc733cc7b11b0
Robert <robertbroersma@gmail.com>
2024-07-29 09:20:30 +0200

    Make use of initial SSR data

--- frontend/pages/collections/index.vue
+++ frontend/pages/collections/index.vue
@@ -1,101 +1,117 @@
 <template>
-    <Card>
-        <CardHeader>
-            <CardTitle>Collections</CardTitle>
-            <CardDescription>List of data sets collections, which you are allowed to edit</CardDescription>
-        </CardHeader>
-        <CardContent>
-            <div class="flex justify-end">
-                <Button>
-                    <NuxtLink to="/collections/create">Add collection</NuxtLink>
-                </Button>
-            </div>
-            <DataTable :columns="collectionColumns" :data="data"/>
-        </CardContent>
-    </Card>
+  <Card>
+    <CardHeader>
+      <CardTitle>Collections</CardTitle>
+      <CardDescription
+        >List of data sets collections, which you are allowed to
+        edit</CardDescription
+      >
+    </CardHeader>
+    <CardContent>
+      <div class="flex justify-end">
+        <Button>
+          <NuxtLink to="/collections/create">Add collection</NuxtLink>
+        </Button>
+      </div>
+      <DataTable
+        v-if="data?.collections"
+        :columns="collectionColumns"
+        :data="data?.collections"
+      />
+    </CardContent>
+  </Card>
 </template>
 
 <script setup lang="ts">
-import DataTable from "@/components/table/DataTable.vue";
-import type {Collection} from "@/lib/collection";
-import {ArrowUpDown, Pencil, Trash2} from "lucide-vue-next";
-import {Button} from '@/components/ui/button'
-import type {ColumnDef} from "@tanstack/vue-table";
+import DataTable from "@/components/table/DataTable.vue"
+import { ArrowUpDown, Pencil, Trash2 } from "lucide-vue-next"
+import { Button } from "@/components/ui/button"
+import type { ColumnDef } from "@tanstack/vue-table"
+import type { components } from "#open-fetch-schemas/api"
 
-const data = ref<Collection[]>([])
-const {$api} = useNuxtApp()
 const router = useRouter()
-
-onMounted(async () => {
-  await updateUi()
+const { data } = await useApi("/collections", {
+  cache: "no-cache",
+  key: "collectionsPage",
 })
 
-function update(waitTime) {
-    return new Promise(resolve => setTimeout(resolve, waitTime));
-}
-async function updateUi() {
-    await new Promise(r => setTimeout(r, 1000));
-    const res = await $api("/collections")
-    data.value = res.collections
-}
+onMounted(async () => {
+  await new Promise((r) => setTimeout(r, 1000))
+  refreshNuxtData("collectionsPage")
+})
+
 async function updateCollection(id: string) {
-    router.push("/collections/update/"+id)
+  router.push("/collections/update/" + id)
 }
 
 async function deleteCollection(id: string) {
-    router.push("/collections/delete/"+id)
+  router.push("/collections/delete/" + id)
 }
 
-const collectionColumns: ColumnDef<Collection>[] = [
-    {
-        accessorKey: 'title',
-        header: ({column}) => {
-            return h(Button, {
-                variant: 'ghost',
-                onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
-            }, () => ['Title', h(ArrowUpDown, {class: 'ml-2 h-4 w-4'})])
+const collectionColumns: ColumnDef<
+  components["schemas"]["stac_pydantic__api__collection__Collection"]
+>[] = [
+  {
+    accessorKey: "title",
+    header: ({ column }) => {
+      return h(
+        Button,
+        {
+          variant: "ghost",
+          onClick: () => column.toggleSorting(column.getIsSorted() === "asc"),
         },
-        cell: ({row}) => h('div', {class: 'lowercase'}, row.getValue('title')),
+        () => ["Title", h(ArrowUpDown, { class: "ml-2 h-4 w-4" })],
+      )
     },
-    {
-        accessorKey: 'description',
-        header: ({column}) => {
-            return h(Button, {
-                variant: 'ghost',
-                onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
-            }, () => ['Description', h(ArrowUpDown, {class: 'ml-2 h-4 w-4'})])
+    cell: ({ row }) => h("div", { class: "lowercase" }, row.getValue("title")),
+  },
+  {
+    accessorKey: "description",
+    header: ({ column }) => {
+      return h(
+        Button,
+        {
+          variant: "ghost",
+          onClick: () => column.toggleSorting(column.getIsSorted() === "asc"),
         },
-        cell: ({row}) => {
-            return h('div', {class: 'lowercase'}, row.getValue('description'))
-        },
+        () => ["Description", h(ArrowUpDown, { class: "ml-2 h-4 w-4" })],
+      )
     },
-    {
-        id: 'edit',
-        cell: ({row}) => {
-            return h(Button, {
-                variant: 'ghost',
-                onClick: () => {
-                    updateCollection(row.original.id)
-                },
-            }, () => ['Edit', h(Pencil, {class: 'ml-2 h-4 w-4'})])
-        },
+    cell: ({ row }) => {
+      return h("div", { class: "lowercase" }, row.getValue("description"))
     },
-    {
-        id: 'delete',
-        cell: ({row}) => {
-            return h(Button, {
-                variant: 'ghost',
-                onClick: () => {
-                    deleteCollection(row.original.id)
-                },
-            }, () => ['Delete', h(Trash2, {class: 'ml-2 h-4 w-4'})])
+  },
+  {
+    id: "edit",
+    cell: ({ row }) => {
+      return h(
+        Button,
+        {
+          variant: "ghost",
+          onClick: () => {
+            updateCollection(row.original.id)
+          },
         },
+        () => ["Edit", h(Pencil, { class: "ml-2 h-4 w-4" })],
+      )
     },
+  },
+  {
+    id: "delete",
+    cell: ({ row }) => {
+      return h(
+        Button,
+        {
+          variant: "ghost",
+          onClick: () => {
+            deleteCollection(row.original.id)
+          },
+        },
+        () => ["Delete", h(Trash2, { class: "ml-2 h-4 w-4" })],
+      )
+    },
+  },
 ]
-
-
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
