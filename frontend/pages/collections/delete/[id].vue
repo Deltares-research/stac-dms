b670b3cae000e349d3311f7c2faa83bbc5ae2ced
grijzea <andre.grijze@deltares.nl>
2024-07-29 10:57:09 +0200

    use prettier

--- frontend/pages/collections/delete/[id].vue
+++ frontend/pages/collections/delete/[id].vue
@@ -1,47 +1,50 @@
 <template>
-    <CollectionCard  buttonTitle="Delete" card-title="Are you sure you want to delete this collection?" :error="errors" :title="title" :description="description" readonly="true" @update="updateCollection"/>
+  <CollectionCard
+    buttonTitle="Delete"
+    card-title="Are you sure you want to delete this collection?"
+    :error="errors"
+    :title="title"
+    :description="description"
+    readonly="true"
+    @update="updateCollection"
+  />
 </template>
 
-
 <script setup lang="ts">
-
-import CollectionCard from "@/components/collections/CollectionCard.vue";
+import CollectionCard from "@/components/collections/CollectionCard.vue"
 
 const { $api } = useNuxtApp()
 
 const errors = ref("")
-const title = ref("");
+const title = ref("")
 const description = ref("")
 
 const route = useRoute()
-const data = await $api('/collections/{collection_id}', {
-    path: {
-        collection_id: route.params.id
-    }
+const data = await $api("/collections/{collection_id}", {
+  path: {
+    collection_id: route.params.id,
+  },
 })
 title.value = data.title
 description.value = data.description
 
 async function updateCollection() {
-    try {
-        errors.value = ""
-        const data = await $api('/collections/{id}', {
-            method: "DELETE",
-            path: {
-                id: route.params.id
-            },
-        })
-    }catch (e){
-        errors.value = "It was not possible to delete the collection"
-        return
-    }
+  try {
+    errors.value = ""
+    const data = await $api("/collections/{id}", {
+      method: "DELETE",
+      path: {
+        id: route.params.id,
+      },
+    })
+  } catch (e) {
+    errors.value = "It was not possible to delete the collection"
+    return
+  }
 
-    const router = useRouter()
-    router.push("/collections")
-
+  const router = useRouter()
+  router.push("/collections")
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
