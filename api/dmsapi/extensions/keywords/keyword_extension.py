00838b8909db295b36ff5bb94231f3ad962633bc
Nathan Clerkx <nathan@wolk.work>
2024-08-19 17:09:24 +0200

    add facility endpoints

--- api/dmsapi/extensions/keywords/keyword_extension.py
+++ api/dmsapi/extensions/keywords/keyword_extension.py
@@ -4,8 +4,9 @@ from stac_pydantic.shared import MimeTypes
 from sqlalchemy.engine import Engine
 from stac_fastapi.types.extension import ApiExtension
 
-from dmsapi.database.models import (
+from dmsapi.database.models import (  # type: ignore
     ErrorResponse,
+    Facility,
     Keyword,
     Keyword_GroupPublic,
     OKResponse,
@@ -25,8 +26,8 @@ class KeywordExtension(ApiExtension):
         DELETE /facility/{facility_id}
 
     Link and unlink keyword groups to facilities:
-        POST /facility/{facility_id}/keywordgroup/{keywordgroup_id}
-        DELETE /facility/{facility_id}/keywordgroup/{keywordgroup_id}
+        POST /facility_keyworkgroup_link
+        DELETE /facility_keyworkgroup_link
 
     Manage keyword groups:
         POST /keywordgroup
@@ -39,9 +40,9 @@ class KeywordExtension(ApiExtension):
         PUT /keyword/{keyword_id}
         DELETE /keywordgroup/{keywordgroup_id}/keyword/{keyword_id}
 
-    Get all keywords:
-        GET /facility/{facility_id}/keywords
-        GET /keywordgroup/{keywordgroup_id}/keywords
+    Get keywords:
+        GET /keywords?facility_id={facility_id}
+        GET /keywords?keywordgroup_id={keywordgroup_id}
 
     Args:
         ApiExtension (_type_): _description_
@@ -65,12 +66,92 @@ class KeywordExtension(ApiExtension):
             None
         """
         self.router.prefix = app.state.router_prefix
+        self.add_create_facility()
+        self.add_get_facility()
+        self.add_get_facilities()
+        self.add_delete_facility()
         self.add_create_keyword_group()
         self.add_get_keyword_group()
         self.add_delete_keyword_group()
         self.add_create_keyword()
         self.add_get_keyword()
         app.include_router(self.router, tags=["Keyword Extension"])
+
+    def add_create_facility(self):
+        self.router.add_api_route(
+            name="Create Facility",
+            path="/facility",
+            endpoint=self.client.create_facility,
+            response_model=Facility,
+            methods=["POST"],
+        )
+
+    def add_get_facility(self):
+        self.router.add_api_route(
+            name="Get Facility",
+            path="/facility/{facility_id}",
+            response_model=Facility,
+            responses={
+                200: {
+                    "content": {
+                        MimeTypes.json.value: {},
+                    },
+                    "model": Facility,
+                },
+                400: {
+                    "content": {
+                        MimeTypes.json.value: {},
+                    },
+                    "model": ErrorResponse,
+                },
+                404: {
+                    "content": {
+                        MimeTypes.json.value: {},
+                    },
+                    "model": ErrorResponse,
+                },
+            },
+            endpoint=self.client.get_facility,
+            methods=["GET"],
+        )
+
+    def add_get_facilities(self):
+        self.router.add_api_route(
+            name="Get Facilities",
+            path="/facilities",
+            response_model=list[Facility],
+            endpoint=self.client.get_facilities,
+            methods=["GET"],
+        )
+
+    def add_delete_facility(self):
+        self.router.add_api_route(
+            name="Delete Facility",
+            path="/facility/{facility_id}",
+            response_model=OKResponse,
+            responses={
+                200: {
+                    "content": {
+                        MimeTypes.json.value: {},
+                    },
+                    "model": OKResponse,
+                },
+                400: {
+                    "content": {
+                        MimeTypes.json.value: {},
+                    },
+                    "model": ErrorResponse,
+                },
+                404: {
+                    "content": {
+                        MimeTypes.json.value: {},
+                    },
+                    "model": ErrorResponse,
+                },
+            },
+            endpoint=self.client.delete_facility,
+            methods=["DELETE"],
+        )
 
     def add_create_keyword_group(self):
         self.router.add_api_route(
