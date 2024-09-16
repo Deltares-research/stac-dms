bf652e45d362a666576a37b48980561e0b7a44cf
Robert <robertbroersma@gmail.com>
2024-09-16 09:43:02 +0200

    Implement Keywords Manager (#18)
    * Implement Keyword Manager
    * Add Keywords button to navigation bar

---
+++ frontend/components/keywords/DeleteFacility.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { useForm } from "vee-validate"
+import { toast } from "../ui/toast"
+import { Trash2 } from "lucide-vue-next"
+
+let { facility_id } = defineProps<{
+  facility_id: string
+}>()
+
+let { $api } = useNuxtApp()
+
+let deleteFacilityForm = useForm()
+
+let deleteFacility = deleteFacilityForm.handleSubmit(async () => {
+  await $api(`/facility/{facility_id}`, {
+    method: "delete",
+    path: {
+      facility_id,
+    },
+  })
+
+  toast({
+    title: "Facility deleted",
+  })
+
+  await navigateTo("/keywords/facilities")
+})
+</script>
+
+<template>
+  <form @submit="deleteFacility">
+    <Button variant="destructive" size="icon" type="submit" class="w-8 h-8">
+      <Trash2 class="w-4 h-4" />
+    </Button>
+  </form>
+</template>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,4 @@
-const animate = require("tailwindcss-animate");
+const animate = require("tailwindcss-animate")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -97,4 +97,4 @@ module.exports = {
     },
   },
   plugins: [animate],
-};
+}
