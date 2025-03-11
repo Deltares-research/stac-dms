2fa4df2d5bccddb2bf458ee39b44e2cb4881c49f
florislangeraert <floris.langeraert@deltares.nl>
2025-03-11 13:15:12 +0100

    Changed security group

--- infra/ecs_tasks.tf
+++ infra/ecs_tasks.tf
@@ -301,6 +301,14 @@ resource "aws_security_group" "backend-ecs" {
   name   = "backend-ecs-sg-${terraform.workspace}"
   vpc_id = aws_vpc.vpc.id
 
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all inbound traffic"    
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
