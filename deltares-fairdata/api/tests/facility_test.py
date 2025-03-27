2637743a7e259ad6ad8ada4ab6ff2ce3b8e506b4
Nathan Clerkx <nathan@wolk.work>
2025-03-27 14:34:21 +0100

    add authentication to tests

--- deltares-fairdata/api/tests/facility_test.py
+++ deltares-fairdata/api/tests/facility_test.py
@@ -1,14 +1,18 @@
 import pytest
-from dmsapi.extensions.keywords.keyword_client import KeywordClient
 from dmsapi.database.models import Facility  # type: ignore
+from dmsapi.extensions.keywords.keyword_client import KeywordClient
 from httpx import AsyncClient
 
 
 # test create facility
 @pytest.mark.asyncio
-async def test_create_facility(app_client: AsyncClient, keyword_client: KeywordClient):
+async def test_create_facility(
+    authenticated_client: AsyncClient, keyword_client: KeywordClient
+):
     facility_name = "test_facility"
-    response = await app_client.post("/facility", json={"name": facility_name})
+    response = await authenticated_client.post(
+        "/facility", json={"name": facility_name}
+    )
     assert response.status_code == 200
     assert response.json()["name"] == facility_name
     assert response.json()["id"] is not None
@@ -17,20 +21,20 @@ async def test_create_facility(app_client: AsyncClient, keyword_client: KeywordC
 
 # test create facility invalid
 @pytest.mark.asyncio
-async def test_create_facility_invalid(app_client: AsyncClient):
-    response = await app_client.post("/facility", json={"name": ""})
+async def test_create_facility_invalid(authenticated_client: AsyncClient):
+    response = await authenticated_client.post("/facility", json={"name": ""})
     assert response.status_code == 400
     assert response.json()["code"] == "RequestValidationError"
 
 
 # test get facility
 @pytest.mark.asyncio
-async def test_update_facility(app_client: AsyncClient, facility: Facility):
-    response = await app_client.get(f"/facility/{facility.id}")
+async def test_update_facility(authenticated_client: AsyncClient, facility: Facility):
+    response = await authenticated_client.get(f"/facility/{facility.id}")
     assert response.status_code == 200
     facility_obj = Facility(**response.json())
     assert facility_obj.name == "test_facility"
-    update_response = await app_client.put(
+    update_response = await authenticated_client.put(
         f"/facility/{facility.id}", json={"name": "updated_facility"}
     )
     assert update_response.status_code == 200
@@ -40,8 +44,8 @@ async def test_update_facility(app_client: AsyncClient, facility: Facility):
 
 # test get facility
 @pytest.mark.asyncio
-async def test_get_facility(app_client: AsyncClient, facility: Facility):
-    response = await app_client.get(f"/facility/{facility.id}")
+async def test_get_facility(authenticated_client: AsyncClient, facility: Facility):
+    response = await authenticated_client.get(f"/facility/{facility.id}")
     assert response.status_code == 200
     facility_obj = Facility(**response.json())
     assert facility_obj.name == "test_facility"
@@ -49,8 +53,8 @@ async def test_get_facility(app_client: AsyncClient, facility: Facility):
 
 # test get all facilities
 @pytest.mark.asyncio
-async def test_get_facilities(app_client: AsyncClient, facility: Facility):
-    response = await app_client.get(f"/facilities")
+async def test_get_facilities(authenticated_client: AsyncClient, facility: Facility):
+    response = await authenticated_client.get("/facilities")
     assert response.status_code == 200
     assert len(response.json()) > 0
     facility_obj = Facility(**response.json()[0])
@@ -59,17 +63,19 @@ async def test_get_facilities(app_client: AsyncClient, facility: Facility):
 
 # test delete facility
 @pytest.mark.asyncio
-async def test_delete_facility(app_client: AsyncClient, keyword_client: KeywordClient):
+async def test_delete_facility(
+    authenticated_client: AsyncClient, keyword_client: KeywordClient
+):
     facility_name = "test_facility_to_delete"
     facility = keyword_client.create_facility({"name": facility_name})
 
     # check if facility is created
-    response = await app_client.get(f"/facility/{facility.id}")
+    response = await authenticated_client.get(f"/facility/{facility.id}")
     assert response.status_code == 200
 
     # delete facility
-    response = await app_client.delete(f"/facility/{facility.id}")
+    response = await authenticated_client.delete(f"/facility/{facility.id}")
     assert response.status_code == 200
     assert response.json() == {"message": "Facility deleted"}
-    facility_json = await app_client.get(f"/facility/{facility.id}")
+    facility_json = await authenticated_client.get(f"/facility/{facility.id}")
     assert facility_json.status_code == 404
