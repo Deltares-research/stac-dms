7fc47975b8609dd41236e141b46a76eac82410a1
IoannaMi <59479531+IoannaMi@users.noreply.github.com>
2025-09-25 16:36:02 +0200

    WIP: add store parameters for the filters

--- deltares-fairdata/frontend-migration/stores/searchPage.js
+++ deltares-fairdata/frontend-migration/stores/searchPage.js
@@ -1,17 +1,71 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import searchBody from '@/utils/search/searchBody.js' // the modular builder you made
+import { useNuxtApp } from '#app'
+
 
 export const useSearchPageStore = defineStore('searchPage', () => {
+  //State
   const q = ref('')
-  const start = ref(undefined)
-  const end = ref(undefined)
+  const startDate = ref(undefined)
+  const endDate = ref(undefined)
   const keywords = ref([])
   const collections = ref([])
   const includeEmptyGeometry = ref(false)
   const bbox = ref([ 180, 90, -180, -90 ]) //live TODO: check if both are needed.
   const bboxFilter = ref([ 180, 90, -180, -90 ]) // send in the request. 
 
-  return { q, start, end, keywords, collections, includeEmptyGeometry, bbox, bboxFilter }
+  
+  const featureCollection = ref(null)
+  //getter.
+  //filteredCollections;
+  //
+  const searchStatus = ref('idle') // 'idle' | 'pending' | 'success' | 'error'
+  const searchError = ref(null)
+
+
+  //Functions
+  async function search() {
+    console.log('SEARCH action:', q.value, startDate.value, endDate.value, keywords.value, collections.value, includeEmptyGeometry.value, bboxFilter.value)
+    searchStatus.value = 'pending'; searchError.value = null
+    const { $api } = useNuxtApp()
+    try {
+
+      const { data } = await $api('/search', {
+        method: 'POST',
+        body: searchBody({
+          q: q.value,
+          startDate: startDate.value,
+          endDate: endDate.value,
+          keywords: keywords.value,
+          collections: collections.value,
+          includeEmptyGeometry: includeEmptyGeometry.value,
+          bbox: bboxFilter.value, 
+        }),
+        credentials: 'include',
+        server: false,
+      
+        onRequest ({ request, options }) {
+          console.log('[search:onRequest]', request, options)
+        },
+        onResponse ({ request, response }) {
+          console.log('[search:onResponse]', response.status, response.statusText)
+          // _data is the parsed JSON response
+          console.log('[search:data]', response?._data)
+        },
+        onResponseError ({ request, response }) {
+          console.warn('[search:onResponseError]', response?.status, response?._data)
+        },
+      })
+      featureCollection.value = data.value
+      searchStatus.value = 'success'
+      
+    }catch (e) {
+      searchError.value = e
+      searchStatus.value = 'error'
+    }
+  }
+  return { q, startDate, endDate, keywords, collections, includeEmptyGeometry, bbox, bboxFilter, featureCollection, searchStatus, searchError, search }
 
 
 })
